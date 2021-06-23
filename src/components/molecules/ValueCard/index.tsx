62349c694dec0f72aa97c7b5f586a196ee466a80
Jucielly Silva <juciellysouzasilva@gmail.com>
2021-06-23 16:13:25 -0300

    feat: Value card component created

---
+++ src/components/molecules/ValueCard/index.tsx
@@ -0,0 +1,31 @@
+import React, { ReactNode } from 'react';
+import Skeleton from 'react-loading-skeleton';
+import { StyledValueCard } from './style';
+
+interface ValueCardProps {
+  title: string;
+  icon: ReactNode;
+  value: number;
+  loading: boolean;
+}
+
+const ValueCard: React.FC<ValueCardProps> = ({
+  title,
+  icon,
+  value,
+  loading,
+}) => {
+  return (
+    <StyledValueCard>
+      <div className="container">
+        <h1>{title}</h1>
+        <span className="value">
+          {loading ? <Skeleton height={25} /> : value}
+        </span>
+      </div>
+      <div className="icon">{icon}</div>
+    </StyledValueCard>
+  );
+};
+
+export default ValueCard;

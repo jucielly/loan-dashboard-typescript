1cd58e1e0f068c34719942b362d7ad5d312295ae
Jucielly Silva <juciellysouzasilva@gmail.com>
2021-06-23 13:58:04 -0300

    feat: Input component created

---
+++ src/components/atoms/input/index.tsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+const Input = styled.input`
+  color: ${(props) => props.theme.colors.fontColor};
+  text-align: center;
+  width: 300px;
+  height: 40px;
+  border-radius: 20px;
+  border: 1px solid ${(props) => props.theme.colors.secondary};
+  transition: 0.3s;
+  margin-bottom: 10px;
+  &:focus {
+    border: 1px solid ${(props) => props.theme.colors.terciary};
+  }
+  &::placeholder {
+    color: ${(props) => props.theme.colors.helper};
+  }
+  &:hover {
+    border: 1px solid ${(props) => props.theme.colors.helper};
+    cursor: pointer;
+  }
+`;
+
+export default Input;
